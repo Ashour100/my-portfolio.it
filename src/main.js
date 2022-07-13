9c5155d850872fc7c516cd2914c4fc3b028ed4a4
Ashour100 <interinstitute5@gmail.com>
2022-07-13 16:11:47 +0200

    Pages added

--- src/main.js
+++ src/main.js
@@ -1,8 +1,35 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
+import About from './pages/About'
+import Projects from './pages/projects'
+import Contact from './pages/Contact'
+const router = new VueRouter({
+  mode:'history',
+  routes:[
+      {
+          path:'/about',
+          name:'about',
+          component:About
+      },
+      {
+        path:'/projects',
+        name:'projects',
+        component:Projects
+      },
+      {
+        path:'/contact',
+        name:'contact',
+        component:Contact
+      },
+]
+})
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
