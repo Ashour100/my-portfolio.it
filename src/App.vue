d51ad7251816342e9650ccd1acf96ca316f4ab05
Ashour100 <interinstitute5@gmail.com>
2022-07-13 16:50:33 +0200

    Bootstrap installed

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,7 @@ export default {
 </script>
 
 <style lang="scss">
+@import "~bootstrap/scss/bootstrap";
 // #app {
 //   font-family: Avenir, Helvetica, Arial, sans-serif;
 //   -webkit-font-smoothing: antialiased;
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>About-Page</h1>
+        <h1 class="text-center">About-Page</h1>
     </div>
 </template>
 
